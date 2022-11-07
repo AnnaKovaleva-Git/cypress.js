60847ebe107a2686d5fbefdcaf62182caa8d9a43
Kovalev <Kovalev.K@citilink.ru>
2022-11-07 15:18:17 +0300

    init

---
+++ cypress/integration/categories1.spec.js
@@ -0,0 +1,14 @@
+describe('Позитивный кейс', () => {
+    it('Категории', () => {
+        cy.visit('https://edadeal.ru/moskva')
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+        })
+        cy.wait(5000);
+        cy.get('.b-header__search-input').type('для дома')
+        cy.get('.b-header__search-button').click()
+        cy.contains('Сортировка:')
+    })
+})
---
+++ cypress/integration/information1.spec.js
@@ -0,0 +1,14 @@
+describe('Позитивный кейс', () => {
+    it('Информационный', () => {
+        cy.visit('https://edadeal.ru/moskva')
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+        })
+        cy.wait(5000);
+        cy.get('.b-header__search-input').type('рис')
+        cy.get('.b-header__search-button').click()
+        cy.contains('Сортировка:')
+    })
+})
